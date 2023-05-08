0e68cb23d62502e17d2d76eac713bc394e5c75c8
NatashaKhotkina <natashakhotkina@gmail.com>
2023-05-08 21:58:10 +0400

    Updated train function.

--- src/train.py
+++ src/train.py
@@ -85,43 +85,6 @@ def train_epoch_multi(model, trainload, epoch, criterion, loss_hist,
             print(f"Epoch={epoch} loss={loss_hist[antibody][epoch]}, antibody={antibody}")
 
 
-# def train_epoch_multi_equal(model, trainload, epoch, criterion, loss_hist,
-#                       optimizer, print_epoch, device, targeted_AB):
-#     hist_loss = defaultdict(float)
-#     iterators = {antibody: iter(loader)
-#                  for antibody, loader in trainload.items()}
-#     is_over = {'ly555': False,
-#                'ly16': False,
-#                'REGN33': False,
-#                'REGN87': False}
-#
-#     while not all(is_over.values()):
-#         for antibody, loader_iter in iterators.items():
-#             try:
-#                 features, labels = next(loader_iter)
-#             except StopIteration:
-#                 is_over[antibody] = True
-#                 continue
-#             features = features.to(device)
-#             labels = labels.to(device)
-#             # sets the gradients of all optimized tensors to zero.
-#             optimizer.zero_grad()
-#             # get outputs
-#             outputs = model(features, antibody)
-#             # calculate loss
-#             loss = criterion(outputs, labels.unsqueeze(1))
-#             # calculate gradients
-#             loss.backward()
-#             # performs a single optimization step (parameter update).
-#             optimizer.step()
-#             hist_loss[antibody] += loss.item()
-#
-#     if print_epoch:
-#         for antibody in iterators:
-#             loss_hist[antibody].append(hist_loss[antibody] / len(trainload[antibody]))
-#             print(f"Epoch={epoch} loss={loss_hist[antibody][epoch]}, antibody={antibody}")
-
-
 def train_multi_model(model, trainload, num_epochs=20, learning_rate=0.001, criterion=nn.BCEWithLogitsLoss,
                       optim=torch.optim.Adam, print_epoch=False, device='cpu', targeted_AB='ly16',
                       target_num_epochs=0):
