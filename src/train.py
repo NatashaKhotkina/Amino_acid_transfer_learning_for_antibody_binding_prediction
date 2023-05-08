47d72f53a9cfd330e8178fbdaf11994d89155be5
NatashaKhotkina <natashakhotkina@gmail.com>
2023-05-08 17:03:45 +0400

    Updated train function.

--- src/train.py
+++ src/train.py
@@ -85,41 +85,41 @@ def train_epoch_multi(model, trainload, epoch, criterion, loss_hist,
             print(f"Epoch={epoch} loss={loss_hist[antibody][epoch]}, antibody={antibody}")
 
 
-def train_epoch_multi_equal(model, trainload, epoch, criterion, loss_hist,
-                      optimizer, print_epoch, device, targeted_AB):
-    hist_loss = defaultdict(float)
-    iterators = {antibody: iter(loader)
-                 for antibody, loader in trainload.items()}
-    is_over = {'ly555': False,
-               'ly16': False,
-               'REGN33': False,
-               'REGN87': False}
-
-    while not all(is_over.values()):
-        for antibody, loader_iter in iterators.items():
-            try:
-                features, labels = next(loader_iter)
-            except StopIteration:
-                is_over[antibody] = True
-                continue
-            features = features.to(device)
-            labels = labels.to(device)
-            # sets the gradients of all optimized tensors to zero.
-            optimizer.zero_grad()
-            # get outputs
-            outputs = model(features, antibody)
-            # calculate loss
-            loss = criterion(outputs, labels.unsqueeze(1))
-            # calculate gradients
-            loss.backward()
-            # performs a single optimization step (parameter update).
-            optimizer.step()
-            hist_loss[antibody] += loss.item()
-
-    if print_epoch:
-        for antibody in iterators:
-            loss_hist[antibody].append(hist_loss[antibody] / len(trainload[antibody]))
-            print(f"Epoch={epoch} loss={loss_hist[antibody][epoch]}, antibody={antibody}")
+# def train_epoch_multi_equal(model, trainload, epoch, criterion, loss_hist,
+#                       optimizer, print_epoch, device, targeted_AB):
+#     hist_loss = defaultdict(float)
+#     iterators = {antibody: iter(loader)
+#                  for antibody, loader in trainload.items()}
+#     is_over = {'ly555': False,
+#                'ly16': False,
+#                'REGN33': False,
+#                'REGN87': False}
+#
+#     while not all(is_over.values()):
+#         for antibody, loader_iter in iterators.items():
+#             try:
+#                 features, labels = next(loader_iter)
+#             except StopIteration:
+#                 is_over[antibody] = True
+#                 continue
+#             features = features.to(device)
+#             labels = labels.to(device)
+#             # sets the gradients of all optimized tensors to zero.
+#             optimizer.zero_grad()
+#             # get outputs
+#             outputs = model(features, antibody)
+#             # calculate loss
+#             loss = criterion(outputs, labels.unsqueeze(1))
+#             # calculate gradients
+#             loss.backward()
+#             # performs a single optimization step (parameter update).
+#             optimizer.step()
+#             hist_loss[antibody] += loss.item()
+#
+#     if print_epoch:
+#         for antibody in iterators:
+#             loss_hist[antibody].append(hist_loss[antibody] / len(trainload[antibody]))
+#             print(f"Epoch={epoch} loss={loss_hist[antibody][epoch]}, antibody={antibody}")
 
 
 def train_multi_model(model, trainload, num_epochs=20, learning_rate=0.001, criterion=nn.BCEWithLogitsLoss,
@@ -132,7 +132,7 @@ def train_multi_model(model, trainload, num_epochs=20, learning_rate=0.001, crit
 
     loss_hist = defaultdict(list)
     for ep in range(num_epochs):
-        train_epoch_multi_equal(model=model, trainload=trainload, epoch=ep, criterion=criterion, loss_hist=loss_hist,
+        train_epoch_multi(model=model, trainload=trainload, epoch=ep, criterion=criterion, loss_hist=loss_hist,
                           optimizer=optimizer, print_epoch=print_epoch, device=device, targeted_AB=targeted_AB)
 
     for ep in range(target_num_epochs):
