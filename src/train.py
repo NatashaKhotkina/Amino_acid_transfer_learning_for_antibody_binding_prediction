a1a914891739e6c2bf68284989b7ef544536691f
NatashaKhotkina <natashakhotkina@gmail.com>
2023-09-17 16:00:24 +0400

    Updated functions for train.

--- src/train.py
+++ src/train.py
@@ -96,7 +96,7 @@ def train_epoch_multi(model, trainload, epoch, criterion, optimizer, train_stat,
             optimizer.step()
             if antibody == targeted_ab:
                 hist_loss += loss.item()
-                roc_auc += roc_auc_score(labels.unsqueeze(1).cpu().numpy(), outputs.cpu().numpy())
+                roc_auc += roc_auc_score(labels.unsqueeze(1).cpu(), outputs.cpu())
 
     if train_stat:
         val_loss, accuracy, precision, recall, f1, val_roc_auc = eval_model(model, testload, criterion,
